22155329fd0bb646af57347a60ab54859abff969
Mészáros Barna <barna.meszaros02@gmail.com>
2024-03-21 13:49:12 +0100

    Skeleton initial setup

---
+++ src/controller/Skeleton.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Skeleton {
+    
+    public static void main(String[] args) {
+            
+    }
+}
---
+++ src/model/IItem.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * IItem
+ */
+public interface IItem {
+
+    
+}
