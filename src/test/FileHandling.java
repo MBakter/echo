3d58cffa4a3d66b18a235441d1a4e06229f8a96a
Csirmaz Gáspár <csirmazg@gmail.com>
2024-04-26 20:44:13 +0200

    more folders

--- src/test/FileHandling.java
+++ src/test/FileHandling.java
@@ -1,5 +1,18 @@
 package test;
 
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class FileHandling {
-    
+
+    public ArrayList<CommandData> ReadTest(String filePathString){
+        Path p = FileSystems.getDefault().getPath("logs", "access.log");
+        //File f = new File();
+        ArrayList<CommandData> list=  new ArrayList<CommandData>();
+        list.add(new CommandData());
+
+        return list;
+    };
 }
