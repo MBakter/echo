8a11a923eb68a6a89c2c561725ebca5c8140e863
Csirmaz Gáspár <csirmazg@gmail.com>
2024-05-20 19:03:00 +0200

    label interface

--- src/view/IVItems.java
+++ src/view/IVItems.java
@@ -1,10 +1,11 @@
 package view;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 import controller.IController;
 
 public interface IVItems {
     void draw(IVStudent curPlayer, JButton btn, IController c);
-    void drawOnGround(IVStudent curPlayer, JButton btn, IController c);
+    void draw(IVStudent curPlayer, JLabel btn, IController c);
 }
